9632741856b918e666c084bf72c7492a6b461bc6
Muhammed Irshad <muhammed irshadismail0722@gmail.com>
2025-10-07 14:15:17 +0200

    added policy page

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -154,6 +154,11 @@ const LoginPage = () => {
               >
                 Login
               </Button>
+              <div className="flex items-center my-4">
+                <div className="flex-grow border-t border-gray-300"></div>
+                <span className="px-3 text-sm text-gray-500 uppercase">or</span>
+                <div className="flex-grow border-t border-gray-300"></div>
+              </div>
               <div className="w-full sm:w-auto">
                 <div className="w-full">
                   {" "}
